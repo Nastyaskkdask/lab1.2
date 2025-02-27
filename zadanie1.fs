f1be9d55fd0bec7c0ad0246938acf3a3d7efa910
Nastyaskkdask <144699427+Nastyaskkdask@users.noreply.github.com>
2025-02-28 04:00:16 +0500

    Add files via upload

---
+++ zadanie1.fs
@@ -0,0 +1,22 @@
+﻿open System
+
+//1
+printf "Введите размерность списка: "
+let n = int(Console.ReadLine())
+printfn "Введите числа: "
+let list = [
+    for i in 0..(n-1) do
+        int(Console.ReadLine())
+        
+    ] 
+
+let list1 = [
+        for i in 0..(n-1) do
+            -list.Item(i)
+    ]        
+
+printfn "Начальный список: %A" list
+printfn "Обратный список: %A" list1
+
+
+
---
+++ zadanie2.fs
@@ -0,0 +1,17 @@
+﻿open System
+//2
+printf "Введите натуральное число: "
+
+let S = string(Console.ReadLine())
+
+let rec t sum i =
+
+    if i = S.Length then
+        printf "Кол-во чётных элементов в числе = %i" sum
+        sum
+    elif int(S[i]) % 2 = 0 then
+        t (sum + 1) (i + 1)
+    else
+        t sum (i + 1)
+
+ 
